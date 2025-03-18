61f3dd6cac64fbc5f6ff7e1847d48170cd04498b
punamcancodee <077bct062.punam@pcampus.edu.np>
2025-03-18 19:29:00 +0545

    commit

--- Learning_From_First/reverse.py
+++ Learning_From_First/reverse.py
@@ -12,3 +12,10 @@ rev = ''
 for i in range(len(str_b)):  #len chae string ma milca but int ma launa mildaina
     rev = str_b[i]+rev
 print(rev)
+
+str_c = 'chettri'
+rev = ''
+for i in range(len(str_c)-1,-1,-1):
+    rev = rev + str_c[i]
+    
+print(rev)
