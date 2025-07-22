3397d438293909e6c501916192f99a0f1af19f4b
Punam Adhikari <punam.adhikari@abacusinsights.com>
2025-07-22 11:15:56 +0545

    Vertocal Scalling

--- LeetcodeProblems/leetcode14.py
+++ LeetcodeProblems/leetcode14.py
@@ -20,10 +20,12 @@ print(longestCommonPrefix((strs = ["flower", "flow", "flight"])))
 
 #vertical scaling
 class Solution:
-    def longestCommonPrefix(self,strs):
+    def longestCommonPrefix(self, strs: List[str]) -> str:
         if strs == None or len(strs) == 0:
             return ""
-        for i in range(len(strs)==0):
+        for i in range(len(strs[0])):
             c = strs[0][i]
-            for j in range(1,len(strs)):
-                if i == len(strs[j])
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != c:
+                    return strs[0][0:i]
+        return strs[0]
