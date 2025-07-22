6785c4c74b29e340541c71ed9e9152d0bd1987f4
Punam Adhikari <punam.adhikari@abacusinsights.com>
2025-07-22 10:40:43 +0545

    Leetcode14

--- LeetcodeProblems/leetcode14.py
+++ LeetcodeProblems/leetcode14.py
@@ -1,4 +1,5 @@
 #Longest Common Prefix
+#horizontalScaling
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
@@ -16,3 +17,13 @@ class Solution(object):
         return prefix
 print(longestCommonPrefix((strs = ["flower", "flow", "flight"])))
 ''' TC = o(s)  s is the sum of all charactes in all strings and sc = o(1)'''
+
+#vertical scaling
+class Solution:
+    def longestCommonPrefix(self,strs):
+        if strs == None or len(strs) == 0:
+            return ""
+        for i in range(len(strs)==0):
+            c = strs[0][i]
+            for j in range(1,len(strs)):
+                if i == len(strs[j])
