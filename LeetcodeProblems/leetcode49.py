0000ed48a80c7f6cfe4f4737e68126db7d599f19
Punam Adhikari <punam.adhikari@abacusinsights.com>
2025-07-23 10:28:15 +0545

    group Anagaram

---
+++ LeetcodeProblems/leetcode49.py
@@ -0,0 +1,28 @@
+#using sorting
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
+
+'''Tc = o(m*nlogn) and sc = o(m*n) where m is the number of strings and n is the length of longset string'''
+
+
+#using hashtable 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
+
+''' TC = o(m*n) and sc = o(m) for extra space and o(m*n) space for output list'''
